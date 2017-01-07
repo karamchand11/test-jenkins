94d4b1d4390d2fa5300d7b64349927042b3a4ebb
karamchand11 <0215k.c@gmail.com>
2017-01-07 15:46:31 +0000

    all

---
+++ a.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+a=5
+if [ $a -gt 4]
+then
+echo "a is grater"
+else 
+echo "a is smaler"
+exit 1
+fi
+
---
+++ b.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+b=10
+if [ $b -gt 4]
+then
+echo "b is grater"
+else 
+echo "b is smaler"
+exit 1
+fi
+
